bd5c1a7893761e7da8a9ddd6b2b82b9dc7cb12f8
elliot <elliotwilliams150@gmail.com>
2026-05-15 20:00:03 +0100

    Backup scripts 2026-05-15 20:00

--- P002_mtscATAC-seq/MitoDriftMaster/mitodrift.sh
+++ P002_mtscATAC-seq/MitoDriftMaster/mitodrift.sh
@@ -5,12 +5,13 @@
 cd /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun040526
 
 Rscript /home/elliot/R/x86_64-pc-linux-gnu-library/4.4/mitodrift/bin/run_mitodrift_em.R \
-  --mut_dat /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/Surg_McgVSAst_mtSDI_sub/AST/AST.csv \
-  --outdir /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/Surg_McgVSAst_mtSDI_sub/AST \
-  --tree_mcmc_iter 10000 \
-  --tree_mcmc_chains 20 \
-  --tree_mcmc_burnin 1000 \
-  --ncores 20 \
+  --mut_dat /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun150526/Combined_Surgical_Lineage_Matrix.csv \
+  --outdir  /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun150526 \
+  --tree_mcmc_iter 100000 \
+  --tree_mcmc_chains 10 \
+  --tree_mcmc_burnin 10000 \
+  --conv_thresh 0.1 \
+  --ncores 10 \
   --ncores_em 30 \
   --ncores_nj 30 \
   --ncores_qs 30 \
