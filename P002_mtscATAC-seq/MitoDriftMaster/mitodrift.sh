160cb88d279157c901ff909ba74011ce0e590ce6
elliot <elliotwilliams150@gmail.com>
2026-05-21 20:00:02 +0100

    Backup scripts 2026-05-21 20:00

--- P002_mtscATAC-seq/MitoDriftMaster/mitodrift.sh
+++ P002_mtscATAC-seq/MitoDriftMaster/mitodrift.sh
@@ -2,18 +2,18 @@
   #module load GLPK  # Loads libglpk for optimization
   #module load R
 
-cd /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun040526
+cd /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun210526
 
 Rscript /home/elliot/R/x86_64-pc-linux-gnu-library/4.4/mitodrift/bin/run_mitodrift_em.R \
-  --mut_dat /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun150526/Combined_Surgical_Lineage_Matrix.csv \
-  --outdir  /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun150526 \
+  --mut_dat /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun210526/combined_mut_dat.csv \
+  --outdir  /mnt/claw-raid/elliot/P002_mtscATAC-seq/MitoDrift/CatRun210526 \
   --tree_mcmc_iter 100000 \
   --tree_mcmc_chains 10 \
   --tree_mcmc_burnin 10000 \
-  --conv_thresh 0.1 \
+  --conv_thres 0.1 \
   --ncores 10 \
-  --ncores_em 30 \
-  --ncores_nj 30 \
-  --ncores_qs 30 \
+  --ncores_em 10 \
+  --ncores_nj 10 \
+  --ncores_qs 10 \
   --ncores_annot 1 \
   --resume FALSE
